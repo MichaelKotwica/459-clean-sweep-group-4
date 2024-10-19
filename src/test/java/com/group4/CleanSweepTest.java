6a69e2eb51a94fac06e7427dfe5ffbf33451426f
MichaelKotwica <45274996+MichaelKotwica@users.noreply.github.com>
2024-10-18 19:14:43 -0500

    made CleanSweepTest.java public

--- src/test/java/com/group4/CleanSweepTest.java
+++ src/test/java/com/group4/CleanSweepTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 //import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class CleanSweepTest {
+public class CleanSweepTest {
 
     int startXTilePos;
     int startYTilePos;
